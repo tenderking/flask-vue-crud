8b7ce4aba83f6ee03e3dcff619939eaad62b9305
tenderking <gmu003@gmail.com>
2022-07-29 01:09:19 +0200

    refactor: moved modal to own component

--- client/src/components/TheModal.vue
+++ client/src/components/TheModal.vue
@@ -1,54 +1,84 @@
 <template>
   <b-modal
-			id="book-modal"
-			title="Add a new book"
-			v-model="modalShow"
-			hide-footer
-		>
-			<b-form @submit="onSubmit" @reset="onReset" class="w-100">
-				<b-form-group
-					id="form-title-group"
-					label="Title:"
-					label-for="form-title-input"
-				>
-					<b-form-input
-						id="form-title-input"
-						type="text"
-						v-model="addBookForm.title"
-						required
-						placeholder="Enter title"
-					>
-					</b-form-input>
-				</b-form-group>
-				<b-form-group
-					id="form-author-group"
-					label="Author:"
-					label-for="form-author-input"
-				>
-					<b-form-input
-						id="form-author-input"
-						type="text"
-						v-model="addBookForm.author"
-						required
-						placeholder="Enter author"
-					>
-					</b-form-input>
-				</b-form-group>
-				<b-form-group id="form-read-group">
-					<b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
-						<b-form-checkbox value="true">Read?</b-form-checkbox>
-					</b-form-checkbox-group>
-				</b-form-group>
-				<b-button type="submit" variant="primary">Submit</b-button>
-				<b-button type="reset" variant="danger">Reset</b-button>
-			</b-form>
-		</b-modal>
+    id="book-modal"
+    class="position-absolute top-50 start-50 translate-middle border border-primary bg-light p-3"
+    title="Add a new book"
+    hide-footer
+    style="z-index: 10o"
+  >
+    <!-- <b-form class="w-100"> -->
+    <form class="w-100" @reset="onReset">
+      <div
+        class="d-flex p-2"
+        id="form-title-group"
+        label="Title:"
+        label-for="form-title-input"
+      >
+        <input
+          id="form-title-input"
+          type="text"
+          :value="titleValue"
+          required
+          placeholder="Enter title"
+          @input="
+						$emit(
+							'update:titleValue',
+							($event.target as HTMLInputElement).value
+						)
+					"
+        />
+      </div>
+      <div
+        class="d-flex p-2"
+        id="form-author-group"
+        label="Author:"
+        label-for="form-author-input"
+      >
+        <input
+          id="form-author-input"
+          type="text"
+          :value="authorValue"
+          @input="
+						$emit(
+							'update:authorValue',
+							($event.target as HTMLInputElement).value
+						)
+					"
+          required
+          placeholder="Enter author"
+        />
+      </div>
+
+      <div class="d-flex ms-4 p-2 gap-3 form-check" id="form-read-group">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          :checked="readValue"
+          @change="
+						$emit('update:readValue', ($event.target as HTMLInputElement).value)
+					"
+        />
+        <label class="form-check-label" for="flexCheckChecked"> Read? </label>
+      </div>
+      <div class="btn-group" role="group">
+        <button class="btn btn-primary" variant="primary" @click="onSubmit()">
+          Submit
+        </button>
+        <button type="reset" class="btn btn-danger" variant="danger">Reset</button>
+      </div>
+    </form>
+  </b-modal>
 </template>
 
 <script lang="ts" setup>
+defineProps({
+  titleValue: { type: String },
+  authorValue: { type: String },
+  readValue: { type: Boolean },
 
+  onSubmit: { type: Function, required: true },
+});
+defineEmits(["update:titleValue", "update:authorValue", "update:readValue"]);
+
+const onReset = () => console.log("reset");
 </script>
-
-<style>
-
-</style>
